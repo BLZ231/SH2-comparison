89013038f14dca53dc10646f6e5835150cd3a6ed
BLZ231 <eriklarson231@gmail.com>
2025-05-14 20:44:39 -0400

    Finished the Pseudocode I'm sure there are things I missed that will need to be addressed later But at least the basic structure is there Now to begin converting things

---
+++ script.js
@@ -0,0 +1,34 @@
+//Function 1
+//Should be something that enables the necessary
+//interactive elements of the main page
+//Main page
+//Have the bottom three buttons grow slightly when the mouse hovers over them
+//Individual growth, not all three growing at once
+
+//Function 2
+//Should be something that enables the necessary
+//interactive elements of page 1 and 2
+//Page 1
+//Have clickable left and right buttons that scroll through
+//each pair of Environment pictures
+//Have clickable left and right buttons that scroll through
+//each pair of Monster pictures
+//Page 2
+//Have clickable left and right buttons that scroll through
+//each pair of Atmosphere pictures
+
+//Function 3
+//Should be something that enables the necessary
+//interactive elements of page 3
+//Page 3
+//Each picture pair should have designated text on it
+//The heading, which is the name of the section and is always visible
+//And lower text describing how each section is paced
+//The lower text should be invisible
+//until the mouse hovers over the picture pair
+
+
+
+
+
+
